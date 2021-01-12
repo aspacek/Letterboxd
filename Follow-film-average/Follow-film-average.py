0f56bb3233e1b1dfc10ccf058a44ec11ccacbe8f
Alex <guitarist_of_ra@pacbell.net>
2021-01-11 19:10:23 -0700

    Edited Follow-film-etc routines to use the relevant newly added general routines.

--- Follow-film-average/Follow-film-average.py
+++ Follow-film-average/Follow-film-average.py
@@ -10,50 +10,14 @@ import requests
 
 import numpy as np
 
+sys.path.insert(0, "../General-league-routines")
+from Findstrings import findstrings
+from Getstrings import getstrings
+
 ##
 ## Written by Alex Spacek
 ## January 2021
 ##
-
-##################################
-def findstrings(substring,string):
-	lastfound = -1
-	while True:
-		lastfound = string.find(substring,lastfound+1)
-		if lastfound == -1:  
-			break
-		yield lastfound
-
-##################################################
-def getstrings(which,prestring,poststring,source):
-	# First find the start of the number of pages string:
-	length = len(prestring)
-	prevalue = list(findstrings(prestring,source))
-	if len(prevalue) > 0:
-		# If first instance wanted:
-		if which == 'first':
-			prevalue = [prevalue[0]+length]
-		# If last instance wanted:
-		elif which == 'last':
-			prevalue = [prevalue[-1]+length]
-		# If all instances wanted:
-		elif which == 'all':
-			prevalue = [item+length for item in prevalue]
-		else:
-			sys.exit('ERROR - in function "getstrings" - Invalid input for "which"')
-	else:
-		return ''
-	# Find the location of the end of string, and get the strings:
-	strings = []
-	for beginning in prevalue:
-		end = source.find(poststring,beginning)
-		value = source[beginning:end]
-		strings = strings+[value]
-	# If just one string desired, return a scalar, otherwise return the array:
-	if which == 'first' or which == 'last':
-		return strings[0]
-	elif which == 'all':
-		return strings
 
 ############################################################################
 ############################################################################
--- Follow-films-toplist/Follow-films-toplist.py
+++ Follow-films-toplist/Follow-films-toplist.py
@@ -10,100 +10,15 @@ import requests
 
 import numpy as np
 
+sys.path.insert(0, "../General-league-routines")
+from Findstrings import findstrings
+from Getstrings import getstrings
+from Numsort import numsort
+
 ##
 ## Written by Alex Spacek
 ## January 2021
 ##
-
-##################################
-def findstrings(substring,string):
-	lastfound = -1
-	while True:
-		lastfound = string.find(substring,lastfound+1)
-		if lastfound == -1:  
-			break
-		yield lastfound
-
-##################################################
-def getstrings(which,prestring,poststring,source):
-	# First find the start of the number of pages string:
-	length = len(prestring)
-	prevalue = list(findstrings(prestring,source))
-	if len(prevalue) > 0:
-		# If first instance wanted:
-		if which == 'first':
-			prevalue = [prevalue[0]+length]
-		# If last instance wanted:
-		elif which == 'last':
-			prevalue = [prevalue[-1]+length]
-		# If all instances wanted:
-		elif which == 'all':
-			prevalue = [item+length for item in prevalue]
-		else:
-			sys.exit('ERROR - in function "getstrings" - Invalid input for "which"')
-	else:
-		return ''
-	# Find the location of the end of string, and get the strings:
-	strings = []
-	for beginning in prevalue:
-		end = source.find(poststring,beginning)
-		value = source[beginning:end]
-		strings = strings+[value]
-	# If just one string desired, return a scalar, otherwise return the array:
-	if which == 'first' or which == 'last':
-		return strings[0]
-	elif which == 'all':
-		return strings
-
-###################################################
-def numsort(arraytosort,arraytomatch,isstring,highestfirst):
-	# Make sure array lengths match:
-	if len(arraytosort) != len(arraytomatch):
-		sys.exit('ERROR - in function "numsort" - Array lengths do not match.')
-	# Initialize final arrays:
-	finalarraytosort = [-1 for i in arraytosort]
-	if isstring == 1:
-		finalarraytomatch = ['-1' for i in arraytomatch]
-	else:
-		finalarraytomatch = [-1 for i in arraytomatch]
-	# Sort with highest value first:
-	if highestfirst == 1:
-		# Loop through everything until sorted:
-		flag = 0
-		loc = 0
-		while flag == 0:
-			# Find max value:
-			maxval = max(arraytosort)
-			# Put all max values next in the final arrays:
-			toremove = []
-			for i in range(len(arraytosort)):
-				if arraytosort[i] == maxval:
-					finalarraytosort[loc] = arraytosort[i]
-					finalarraytomatch[loc] = arraytomatch[i]
-					loc = loc+1
-					toremove = toremove+[i]
-			# Remove values already used, from back to front:
-			toremove.reverse()
-			for i in range(len(toremove)):
-				del arraytosort[toremove[i]]
-				del arraytomatch[toremove[i]]
-			# If arrays are empty, done:
-			if arraytosort == []:
-				flag = 1
-		# Make sure arrays are completely filled:
-		for i in range(len(finalarraytosort)):
-			if finalarraytosort[i] == -1:
-				sys.exit('ERROR - in function "numsort" - Arrays not sorted correctly.')
-			if isstring == 1:
-				if finalarraytomatch[i] == '-1':
-					sys.exit('ERROR - in function "numsort" - Arrays not sorted correctly.')
-			else:
-				if finalarraytomatch[i] == -1:
-					sys.exit('ERROR - in function "numsort" - Arrays not sorted correctly.')
-	# Sort with lowest value first:
-	else:
-		sys.exit('ERROR - in function "numsort" - highestfirst = 0 not implemented yet.')
-	return finalarraytosort,finalarraytomatch
 
 ############################################################################
 ############################################################################
